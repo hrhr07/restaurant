dd104dc541f0baf6815c6892dc10591bd9e15dc9
Hector <hectorrhr07@gmail.com>
2025-11-05 16:40:57 -0600

    config webpack env

--- webpack.common.js
+++ webpack.common.js
@@ -2,18 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
   module: {
     rules: [
       {
@@ -29,5 +18,15 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
